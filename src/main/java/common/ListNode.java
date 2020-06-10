d0dddc3023bfae5b464b528be691334b9e344c11
曾鹏 <2031481080@qq.com>
2020-06-10 09:06:51 +0800

    '添加common模块'

---
+++ src/main/java/common/ListNode.java
@@ -0,0 +1,15 @@
+package common;
+
+/**
+ * 类说明: 链表节点
+ *
+ * @author 曾鹏
+ */
+public class ListNode {
+
+    int val;
+    ListNode next;
+    public ListNode(int val){
+        this.val = val;
+    }
+}
