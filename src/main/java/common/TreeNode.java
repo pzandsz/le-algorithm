3c9bc6b35020c3519bfd6568cb686b9f65d3ab70
曾鹏 <2031481080@qq.com>
2020-06-10 09:05:09 +0800

    'init'

---
+++ src/main/java/common/TreeNode.java
@@ -0,0 +1,17 @@
+package common;
+
+/**
+ * 类说明: 树节点
+ *
+ * @author 曾鹏
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    public TreeNode(int val){
+        this.val = val;
+
+    }
+
+}
