017a985ac95987335bc7f22b73e215d8f87f8eeb
曾鹏 <2031481080@qq.com>
2020-07-07 19:39:11 +0800

    'fix'

--- src/main/java/tree/Codec.java
+++ src/main/java/tree/Codec.java
@@ -24,11 +24,15 @@ public class Codec {
             return "[]";
         }
         queue.addLast(root);
-        while (queue != null){
+        while (queue != null && !queue.isEmpty()){
             TreeNode currentNode = queue.removeFirst();
+
             String tmp = currentNode == null ?  "null" : currentNode.val+"";
 
             res = res + tmp + ",";
+            if(currentNode == null){
+                continue;
+            }
             queue.addLast(currentNode.left);
             queue.addLast(currentNode.right);
         }
@@ -44,17 +48,44 @@ public class Codec {
     public TreeNode deserialize(String data) {
         String dataTmp = data.replace("[","");
         dataTmp=dataTmp.replace("]","");
-
-        String[] split = dataTmp.split(",");
-        for (int i = 0; i < split.length; i++) {
-            System.out.println(split[i]);
+        if(dataTmp.trim().equals("")){
+            return null;
         }
 
-        return null;
+        LinkedList<TreeNode> nodeQueue = new LinkedList<TreeNode>();
+        String[] array = dataTmp.split(",");
+        TreeNode root = new TreeNode(Integer.parseInt(array[0]));
+        nodeQueue.addLast(root);
+        int i=1;
+        int len = array.length;
+        while (nodeQueue != null && !nodeQueue.isEmpty()){
+            TreeNode currentNode = nodeQueue.removeFirst();
+            if(i < len && array[i].equals("null")){
+                currentNode.left = null;
+            }else if(i < len){
+                currentNode.left = new TreeNode(Integer.parseInt(array[i]));
+                nodeQueue.addLast(currentNode.left);
+            }
+
+            if(i+1 < len && array[i+1].equals("null")){
+                currentNode.right = null;
+            }else if(i+1 < len){
+                currentNode.right = new TreeNode(Integer.parseInt(array[i+1]));
+                nodeQueue.addLast(currentNode.right);
+            }
+
+            i = i+2;
+
+        }
+
+        return root;
     }
 
     public static void main(String[] args) {
         Codec codec = new Codec();
-        codec.deserialize("[1,2,3,null,null,4,5]");
+        TreeNode deserialize = codec.deserialize("[1,2,3,null,null,4,5]");
+        String serialize = codec.serialize(deserialize);
+        System.out.println(serialize);
+
     }
 }
