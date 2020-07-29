2612c7914c5f8983718c8eca0dab0f9a47e52c2d
曾鹏 <2031481080@qq.com>
2020-07-29 19:47:05 +0800

    'fix'

--- src/main/java/tree/AddOneRow.java
+++ src/main/java/tree/AddOneRow.java
@@ -1,9 +1,45 @@
 package tree;
 
+import common.TreeNode;
+
 /**
- * 类说明:
+ * 类说明:623在二叉树中添加一行
  *
  * @author zengpeng
  */
 public class AddOneRow {
+
+    public TreeNode addOneRow(TreeNode root, int v, int d) {
+        if (root == null){
+            return root;
+        }
+        if(d == 1) {
+            TreeNode rootNew = new TreeNode(v);
+            rootNew.left = root;
+            return rootNew;
+        }
+        digui(root,v,d);
+        return root;
+    }
+
+    public void digui(TreeNode root, int v, int d) {
+        if (root == null){
+            return ;
+        }
+
+        if(d-1 == 1){
+            TreeNode left = new TreeNode(v);
+            TreeNode right = new TreeNode(v);
+            left.left = root.left;
+            right.right = root.right;
+            root.left = left;
+            root.right = right;
+            return;
+
+        }
+
+        d--;
+        digui(root.left,v,d);
+        digui(root.right,v,d);
+    }
 }
