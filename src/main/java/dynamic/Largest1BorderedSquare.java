ef00a36bbc32f572ed6d3a3179e5a8c454760a11
曾鹏 <2031481080@qq.com>
2020-07-31 14:53:28 +0800

    'fix'

---
+++ src/main/java/dynamic/Largest1BorderedSquare.java
@@ -0,0 +1,12 @@
+package dynamic;
+
+/**
+ * 类说明:1139. 最大的以 1 为边界的正方形
+ * 给你一个由若干 0 和 1 组成的二维网格 grid，
+ * 请你找出边界全部由 1 组成的最大 正方形子网格，
+ * 并返回该子网格中的元素数量。如果不存在，则返回 0。
+ *
+ * @author zengpeng
+ */
+public class Largest1BorderedSquare  {
+}
