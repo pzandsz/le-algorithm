d24fa9e9ec9042340c691267a5011f987f443804
曾鹏 <2031481080@qq.com>
2020-08-04 09:14:49 +0800

    'fix'

---
+++ src/main/java/dynamic/DeleteAndEarn.java
@@ -0,0 +1,13 @@
+package dynamic;
+
+/**
+ * 类说明: 740删除与获得点数 （打家劫舍问题变种）
+ *
+ * @author zengpeng
+ */
+public class DeleteAndEarn {
+    public int deleteAndEarn(int[] nums) {
+
+        return 0;
+    }
+}
