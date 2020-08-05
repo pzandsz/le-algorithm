5e340d5c60c8ef5c9e36bc59ed72da682a3386e1
曾鹏 <2031481080@qq.com>
2020-08-05 19:37:17 +0800

    'fix'

--- src/main/java/dynamic/Rob.java
+++ src/main/java/dynamic/Rob.java
@@ -19,71 +19,37 @@ import java.util.Map;
  *  3   4
  * 5   5
  *
- * 每个节点相连的点有 父节点 左子节点 右子节点
- * 若自上而下的遍历树的话，两种情况
- * 1.若当前节点被删除，其父节点也会被删除，那么dp[i]应该是dp[i-2]+父节点的兄弟节点+当前节点的值
- * 2.若当前节点不被删除，那么dp[i]=dp[i-1]
- * dp公式 dp[i]=Max(dp[i-1],dp[i-2]+sum(i))
- * 其中dp[i]中的i表示二叉树的层数,sum(i)表示第i层节点值之和+当前节点的父亲节点的兄弟节点之和
+ * 每个节点可选择偷或者不偷两种状态，根据题目意思，相连节点不能一起偷
+ *
+ * 当前节点选择偷时，那么两个孩子节点就不能选择偷了
+ * 当前节点选择不偷时，两个孩子节点只需要拿最多的钱出来就行(两个孩子节点偷不偷没关系)
+ * 我们使用一个大小为 2 的数组来表示 int[] res = new int[2] 0 代表不偷，1 代表偷
+ * 任何一个节点能偷到的最大钱的状态可以定义为
+ *
+ * 当前节点选择不偷：当前节点能偷到的最大钱数 = 左孩子能偷到的钱 + 右孩子能偷到的钱
+ * 当前节点选择偷：当前节点能偷到的最大钱数 = 左孩子选择自己不偷时能得到的钱 +
+ *                                         右孩子选择不偷时能得到的钱 + 当前节点的钱数
  * @author zengpeng
  */
 public class Rob {
-    LinkedList<TreeNode> queue = new LinkedList<>();
-    Map<Integer,Integer> map = new HashMap<>();
     public int rob(TreeNode root) {
-
-        if(root == null){
-            return 0;
-        }
-        //第一步，获取每层节点值之和
-
-        queue.addLast(root);
-        int size=0;
-        int level=0;
-        while (!queue.isEmpty()){
-            if(size == 0){
-                size = queue.size();
-                level++;
-            }
-            TreeNode node = queue.removeFirst();
-            if(node==null){
-                size--;
-                continue;
-            }
-            if(map.get(level) == null){
-                map.put(level,node.val);
-            }else {
-                map.put(level,node.val + map.get(level));
-            }
-            queue.addLast(node.left);
-            queue.addLast(node.right);
-            size--;
-
-        }
-        //第二步，计算最优解
-
-        int[] dp = new int[level];
-        for(int i=1;i<level;i++){
-            if(i==1){
-                dp[i] = map.get(i);
-                continue;
-            }
-            if(i==2){
-                dp[i] = Math.max(map.get(i),dp[i-1]);
-                continue;
-            }
-
-            dp[i]=Math.max(dp[i-1],dp[i-2]+map.get(i));
-        }
-
-        return dp[level-1];
+        int[] result = robInternal(root);
+        return Math.max(result[0], result[1]);
     }
 
+    public int[] robInternal(TreeNode root) {
+        if (root == null){
+            return new int[2];
+        }
+        int[] result = new int[2];
 
-//    public int maxValue(TreeNode node,int level){
-//
-//        if(node == null){
-//            return
-//        }
-//    }
+        int[] left = robInternal(root.left);
+        int[] right = robInternal(root.right);
+
+        result[0] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+        result[1] = left[0] + right[0] + root.val;
+
+        return result;
+    }
+
 }
