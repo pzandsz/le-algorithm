12e331dc06daf9b2cbf46e56601b61a939742c9b
曾鹏 <2031481080@qq.com>
2020-08-25 19:53:45 +0800

    'fix'

--- src/main/java/dynamic/KConcatenationMaxSum.java
+++ src/main/java/dynamic/KConcatenationMaxSum.java
@@ -79,4 +79,21 @@ public class KConcatenationMaxSum {
         }
         return max;
     }
+
+
+
+
+    public int kConcatenationMaxSum2(int[] arr, int k) {
+        if (arr == null || arr.length == 0) return 0;
+        long maxOfEnd = arr[0] > 0 ? arr[0] : 0L, maxSoFar = maxOfEnd, sum = arr[0];
+        for (int i = 1; i < Math.min(k, 2) * arr.length; i++) {
+            maxOfEnd = Math.max(maxOfEnd + arr[i % arr.length], arr[i % arr.length]);
+            maxSoFar = Math.max(maxOfEnd, maxSoFar);
+            if (i < arr.length) sum += arr[i];
+        }
+        while (sum > 0 && --k >= 2)
+            maxSoFar = (maxSoFar + sum) % 1000000007;
+        return (int) maxSoFar;
+
+    }
 }
